8a10dd45ec4c6b63cc1e9047359069707ba894d9
bbutterm <54075794+bbutterm@users.noreply.github.com>
2024-03-17 21:29:46 +0300

    123

--- client/src/scenes/GameScene.js
+++ client/src/scenes/GameScene.js
@@ -167,6 +167,9 @@ export class GameScene extends Phaser.Scene {
         if (this.calculateScore(this.playerHand) > 21) {
             this.determineWinner();
         }
+        if (this.calculateScore(this.playerHand) === 21) {
+            this.playerStand();
+        }
     }
 
     playerStand() {
--- client/src/scenes/MenuScene.js
+++ client/src/scenes/MenuScene.js
@@ -6,14 +6,11 @@ export class MenuScene extends Phaser.Scene {
     create() {
         // Задаем фон
         const user = Telegram.WebApp.initDataUnsafe.user || { id: '0', first_name: 'Guest', score: 100 };
-
+        this.cameras.main.setBackgroundColor('#35654d');
         // Отображение приветствия и очков
-        this.add.text(10, 10, `Привет, ${user.first_name}! Очки: ${user.score}`, { fontSize: '32px', fill: '#FFF' });
+        this.add.text(10, 10, `Привет, ${user.first_name}!`, { fontSize: '32px', fill: '#FFF' });
 
         // Создание кнопки "Новая игра"
-        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Новая игра', { font: '24px Arial', fill: '#FFF' })
-            .setInteractive()
-            .on('pointerdown', () => this.scene.start('GameScene', { userId: user.id, score: user.score }));
         // Создаем кнопку "Играть"
         const playButton = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Играть', { font: '50px Arial', fill: '#fff', backgroundColor: '#8B4513' })
             .setPadding(10, 10, 10, 10)
@@ -25,7 +22,7 @@ export class MenuScene extends Phaser.Scene {
             .setOrigin(0.5);
 
         // Опционально: добавляем текст счета игрока
-        const scoreText = this.add.text(this.cameras.main.width - 200, this.cameras.main.height - 50, 'Score', { fontSize: '32px', fill: '#FFF' }).setOrigin(0.5);
+        //const scoreText = this.add.text(this.cameras.main.width - 200, this.cameras.main.height - 50, 'Score', { fontSize: '32px', fill: '#FFF' }).setOrigin(0.5);
     }
 }
 
